9e2318242f5d6113aa1ae313e0d67fc334ad6103
Arun Isharwal <arunisharwal@gmail.com>
2026-02-12 21:57:54 +0530

    frontened ui

--- FRONTEND/src/components/PageContainer.jsx
+++
@@ -1,9 +0,0 @@
-export default function PageContainer({ children }) {
-  return (
-    <div className="min-h-screen bg-slate-900 text-white flex justify-center">
-      <div className="w-full max-w-md p-6 space-y-6">
-        {children}
-      </div>
-    </div>
-  );
-}
